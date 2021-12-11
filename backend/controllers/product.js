267864533aea649bae7217deb598f5bae822bdd8
richie48 <57833496+richie48@users.noreply.github.com>
2021-12-11 23:29:40 +0100

    added more functions in the product controllers

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -1,5 +1,33 @@
 
+//will need to use my model in the controllers
+const Product= require('../models/Product')
+
 //add products controller.
 exports.addProduct= async (req,res,err)=>{
-    res.send('add product controller is working')
+    try{
+        const product = await Product.create(req.body) 
+
+        res.status(201).json(product)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+
+
+//get products controller.
+exports.getProduct= async (req,res,err)=>{
+    try{
+        const products= await Product.find()
+        res.status(201).json(products)        
+    }catch(err){
+        console.log(err)
+    }
+
+    res.send('get product controller is working')
+}
+
+//update products controller.
+exports.updateProduct= async (req,res,err)=>{
+    res.send('update product controller is working')
 }
--- backend/models/Product.js
+++ backend/models/Product.js
@@ -51,7 +51,6 @@ const productSchema= new mongoose.Schema({
     user: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
-        required: true,
       },
 
 
