7b97e352c2ca33bb25e77ead63b3b09db43a430c
richie48 <57833496+richie48@users.noreply.github.com>
2021-12-13 17:07:12 +0100

    new addition

--- backend/index.js
+++ backend/index.js
@@ -14,5 +14,11 @@ app.use(express.json());
 
 app.listen(5000, console.log(`server is running on port ${process.env.NODE_ENV}`));
 
+//Handling promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`error:${err.message}`);
+    server.close(() => process.exit(1));
+  });
+
 //mount a route
 app.use('/api/v1/products',productRoute);
