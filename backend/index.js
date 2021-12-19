e08dcee21a70e2aa89653725dd605ebc8374ea4e
richie48 <57833496+richie48@users.noreply.github.com>
2021-12-19 13:48:55 +0100

    more updates

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,10 @@ const dotenv=require('dotenv')
 
 const productRoute = require('./routes/product')
 const userRoute = require('./routes/user')
+
+// const morgan= require('./middlewares/morgan')
+const morgan=require('morgan')
+
 dotenv.config({ path: './config/config.env' });
 
 const connectDb=require('./config/db')
@@ -14,6 +18,21 @@ const app= express();
 app.use(express.json());
 
 server=app.listen(5000, console.log(`server is running on port ${process.env.NODE_ENV}`));
+
+//used a 3rd party middleware.
+if (process.env.NODE_ENV==='development'){
+  // app.use(morgan('dev'))
+  app.use(morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      tokens['response-time'](req, res), 'ms'
+    ].join(' ')
+  }))
+}
+
 
 //Handling promise rejection
 process.on('unhandledRejection', (err, promise) => {
