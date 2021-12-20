238527e8b18d78755c911cbb05673a1922b98925
richie48 <57833496+richie48@users.noreply.github.com>
2021-12-20 13:34:22 +0100

    added javascipt error response class

---
+++ backend/utils/errorResponse.js
@@ -0,0 +1,10 @@
+
+//javascript custom error class.
+class ErrorResponse extends Error {
+    constructor(message, statusCode) {
+      //Because we are getting the message from the base class,we need to use super
+      super(message);
+      this.statusCode = statusCode;
+    }
+  }
+  module.exports = ErrorResponse;
