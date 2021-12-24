d410003411e4d0a4ec5cd1d2ac3390995bea401b
richie48 <57833496+richie48@users.noreply.github.com>
2021-12-24 23:49:00 +0100

    fixed set header bug

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -5,45 +5,43 @@ const errorResponse=require('../utils/errorResponse')
 const Product= require('../models/Product')
 
 //add products controller.
-exports.addProduct= asyncHandler(async (req,res,err)=>{
+exports.addProduct= asyncHandler(async (req,res,next)=>{
 
         const product = await Product.create(req.body) 
 
         res.status(201).json(product)
 
-        next(err)
 });
 
 
 
 //get products controller.
-exports.getProduct= asyncHandler(async (req,res,err)=>{
+exports.getProduct= asyncHandler(async (req,res,next)=>{
 
         const products= await Product.find()
         res.status(201).json(products)        
     
-        next(err)
 });
 
 //get a product controller
-exports.getaProduct= asyncHandler(async (req,res,err)=>{
+exports.getaProduct= asyncHandler(async (req,res,next)=>{
         const id=req.params.id
         if (!id){
-            next(new errorResponse("can't get this product",404));
+            return next(new errorResponse("can't get this product",404));
         }
         const products= await Product.findById(req.params.id)
-        res.status(201).json(products)        
     
-        next(err)
+        res.status(201).json(products)   
+        // next(new errorResponse("can't get this product",404));
 })
 
 //update products controller.
-exports.updateProduct= asyncHandler(async (req,res,err)=>{
+exports.updateProduct= asyncHandler(async (req,res,next)=>{
     req.id=req.params.id
     //will need to work here, when updating we have to pass in all the required fields to prevent it from changing to none
     const body=req.body;
     if (!body){
-        next(new errorResponse("Nothing passed in the body of request",404));
+        return next(new errorResponse("Nothing passed in the body of request",404));
     }
     const product= await Product.findByIdAndUpdate(req.params.id,body,{
         new: true,
@@ -51,16 +49,16 @@ exports.updateProduct= asyncHandler(async (req,res,err)=>{
           })
 
     res.status(200).json(product);
-
-    next(err)
 });
 
 // delete a product
-exports.deleteProduct = asyncHandler(async (req, res, err) => {
+exports.deleteProduct = asyncHandler(async (req, res,next) => {
     const product = await Product.findById(req.params.id);
+
     if (!product) {
-        next(new errorResponse('Product does not exist'))
+        return next(new errorResponse('Product does not exist'))
         }
+        
     product.remove()
     res.status(200).json(product);
-    next(err)});
+});
